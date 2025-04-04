8af5bb16f0754c6970164733ce6566c4b4c3f243
Sahrul Ramdan <sahrulramdan.75@gmail.com>
2025-04-04 21:24:39 +0700

    Add ZSH configuration with oh-my-zsh and plugins

--- modules/packages.nix
+++ modules/packages.nix
@@ -3,6 +3,7 @@
 {
   # Define packages to be installed for the user via Home Manager
   home.packages = with pkgs; [
+    nixd
     zellij
     bat
     fzf
--- modules/zsh.nix
+++ modules/zsh.nix
@@ -1,15 +1,20 @@
 { config, pkgs, lib, ... }: {
   programs.zsh = {
     enable = true;
-    enableSyntaxHighlighting = true;
     enableAutosuggestions = true;
+    enableSyntaxHighlighting = true;
+    shellAliases = {
+      lg = "lazygit";
+      ydev = "yarn dev";
+      bdev = "bun dev";
+      nprdev = "npm run dev";
+      zl = "zellij";
+    };
 
     oh-my-zsh = {
       enable = true;
-      plugins = [ "git" "docker" "fzf" ];
+      plugins = [ "git" "docker" "fzf" "you-should-use" ];
       theme = "robbyrussell";
     };
-
-    initExtra = '''';
   };
 }
